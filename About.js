f83f3ce042a3210be644e91c6d338551be4f5598
Ryanchoiseunghyun <144211077+Ryanchoiseunghyun@users.noreply.github.com>
2023-12-06 21:32:22 +0900

    Add files via upload

---
+++ About.js
@@ -0,0 +1,11 @@
+function openSidebar() {
+    document.querySelector('.sidebar').style.width = '250px';
+    document.querySelector('.content').style.marginLeft = '250px';
+    document.querySelector('.content').target = 'show';
+  }
+  
+  function closeSidebar() {
+    document.querySelector('.sidebar').style.width = '0';
+    document.querySelector('.content').style.marginLeft = '0';
+    document.querySelector('.content').target = 'hidden';
+  }
